d38d03c89e65b98fb651e8db95da047c06e694d8
Florian Hartnagel <Florian.Hartnagel@gmx.de>
2023-02-03 16:39:01 +0100

    Run start with bootstrap

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 if [ -e "installer" ]; then export PATH="$(pwd)/installer/bin:$PATH"; fi
 
 # Setup the packages required for the installer
-scripts/bootstrap.sh || exit 1
+./bootstrap.sh || exit 1
 
 # set new installer's PATH, if it downloaded any packages
 if [ -e "installer_files/env" ]; then export PATH="$(pwd)/installer_files/env/bin:$PATH"; fi
@@ -19,4 +19,4 @@ which conda
 conda --version || exit 1
 
 # Download the rest of the installer and UI
-scripts/on_env_start.sh
+# scripts/on_env_start.sh
